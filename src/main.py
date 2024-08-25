4449d0f2ce2125b9a8abf0148c834597bc1b20ee
Marc Singleton <marcsingleton@berkeley.edu>
2024-08-25 08:55:28 -0700

    Put main routine under main guard

--- src/main.py
+++ src/main.py
@@ -133,12 +133,12 @@ def epidemic():
     shared.infection_deck.intensify()
 
 
-# INITIALIZATION
-# Basic Game Parameters
+# SETTINGS
+# Basic Game Settings
 player_names = ['marc', 'ben', 'joseph']  # Cannot exceed 4 names
 epidemic_num = 5  # Cannot exceed 6
 
-# Advanced Game Parameters
+# Advanced Game Settings
 layout = layouts.default
 start_city = 'atlanta'
 outbreak_max = 8
@@ -146,84 +146,86 @@ infection_seq = [2, 2, 2, 3, 3, 4, 4]
 cube_num = 24
 station_num = 6
 
-# Computed Game Parameters
-player_num = len(player_names)
-start_hand_num = 6 - player_num
+if __name__ == '__main__':
+    # INITIALIZATION
+    # Computed Game Settings
+    player_num = len(player_names)
+    start_hand_num = 6 - player_num
 
-# Count unique disease colors
-colors = set([attrs.color for attrs in layout.values()])
+    # Count unique disease colors
+    colors = set([attrs.color for attrs in layout.values()])
 
-# Instantiate diseases
-for color in colors:
-    shared.diseases[color] = pieces.Disease(color, cube_num)
+    # Instantiate diseases
+    for color in colors:
+        shared.diseases[color] = pieces.Disease(color, cube_num)
 
-# Instantiate trackers
-shared.outbreak_track = pieces.OutbreakTrack(outbreak_max)
-shared.infection_track = pieces.InfectionTrack(infection_seq)
+    # Instantiate trackers
+    shared.outbreak_track = pieces.OutbreakTrack(outbreak_max)
+    shared.infection_track = pieces.InfectionTrack(infection_seq)
 
-# Instantiate cities and associated cards
-city_cards = []
-infection_cards = []
-pieces.City.stations = station_num
-for city, attrs in layout.items():
-    shared.cities[city] = pieces.City(city, attrs.neighbors, attrs.color, colors)
-    city_cards.append(cards.CityCard(city, attrs.color, attrs.population))
-    infection_cards.append(cards.InfectionCard(city, attrs.color))
-shared.cities[start_city].add_station()  # Add research station to start city
+    # Instantiate cities and associated cards
+    city_cards = []
+    infection_cards = []
+    pieces.City.stations = station_num
+    for city, attrs in layout.items():
+        shared.cities[city] = pieces.City(city, attrs.neighbors, attrs.color, colors)
+        city_cards.append(cards.CityCard(city, attrs.color, attrs.population))
+        infection_cards.append(cards.InfectionCard(city, attrs.color))
+    shared.cities[start_city].add_station()  # Add research station to start city
 
-# Instantiate decks
-shared.player_deck = cards.PlayerDeck(city_cards + cards.event_cards)
-shared.infection_deck = cards.InfectionDeck(infection_cards)
+    # Instantiate decks
+    shared.player_deck = cards.PlayerDeck(city_cards + cards.event_cards)
+    shared.infection_deck = cards.InfectionDeck(infection_cards)
 
-# Instantiate players
-role_list = list(roles.roles)
-shuffle(role_list)
-for player in player_names:
-    role = role_list.pop()
-    shared.players[player] = roles.roles[role](player)
-    starting_cards = [shared.player_deck.draw() for _ in range(start_hand_num)]
-    for card in starting_cards:
-        shared.players[player].add_card(card)
-
-# Infect cities
-for _ in range(3):
-    for i in range(3, 0, -1):
-        shared.infection_deck.draw(i, verbose=False)
-
-# Add epidemics
-shared.player_deck.add_epidemics(epidemic_num)
-
-# Set turns and initial positions
-turn_count = 0
-player_names = turn_order(player_names)
-for player in shared.players.values():
-    player.city = start_city  # Set separately from instantiation so special abilities do not interfere with setup
-
-# PLAY
-while True:
-    # Turn setup
-    turn = turn_count % player_num
-    current_player = shared.players[player_names[turn]]
-    shared.draw_count = 2
-    shared.infect_count = shared.infection_track.rate
-    print_status()
-
-    # Player actions
-    while current_player.action_count > 0:
-        interface({**current_player.actions, 'neighbors': print_neighbors,
-                   'event': play_event, 'status': print_status}, 'Enter your next action: ')
-
-    # Draw cards
-    while shared.draw_count > 0:
-        interface({'draw': draw_player, 'event': play_event, 'status': print_status}, 'Draw or play event card: ')
-        shared.outbreak_track.reset()  # Reset outbreak after each draw
+    # Instantiate players
+    role_list = list(roles.roles)
+    shuffle(role_list)
+    for player in player_names:
+        role = role_list.pop()
+        shared.players[player] = roles.roles[role](player)
+        starting_cards = [shared.player_deck.draw() for _ in range(start_hand_num)]
+        for card in starting_cards:
+            shared.players[player].add_card(card)
 
     # Infect cities
-    while shared.infect_count > 0:
-        interface({'infect': draw_infect, 'event': play_event, 'status': print_status}, 'Infect or play event card: ')
-        shared.outbreak_track.reset()  # Reset outbreak after each draw
+    for _ in range(3):
+        for i in range(3, 0, -1):
+            shared.infection_deck.draw(i, verbose=False)
 
-    # Turn cleanup
-    current_player.reset()
-    turn_count += 1
-    sleep(1)
+    # Add epidemics
+    shared.player_deck.add_epidemics(epidemic_num)
+
+    # Set turns and initial positions
+    turn_count = 0
+    player_names = turn_order(player_names)
+    for player in shared.players.values():
+        player.city = start_city  # Set separately from instantiation so special abilities do not interfere with setup
+
+    # PLAY
+    while True:
+        # Turn setup
+        turn = turn_count % player_num
+        current_player = shared.players[player_names[turn]]
+        shared.draw_count = 2
+        shared.infect_count = shared.infection_track.rate
+        print_status()
+
+        # Player actions
+        while current_player.action_count > 0:
+            interface({**current_player.actions, 'neighbors': print_neighbors,
+                       'event': play_event, 'status': print_status}, 'Enter your next action: ')
+
+        # Draw cards
+        while shared.draw_count > 0:
+            interface({'draw': draw_player, 'event': play_event, 'status': print_status}, 'Draw or play event card: ')
+            shared.outbreak_track.reset()  # Reset outbreak after each draw
+
+        # Infect cities
+        while shared.infect_count > 0:
+            interface({'infect': draw_infect, 'event': play_event, 'status': print_status}, 'Infect or play event card: ')
+            shared.outbreak_track.reset()  # Reset outbreak after each draw
+
+        # Turn cleanup
+        current_player.reset()
+        turn_count += 1
+        sleep(1)
