2c32f51cfbe3ba2031ca6760ea8b05e89af44121
Marc Singleton <marcsingleton@berkeley.edu>
2024-09-09 22:19:00 -0400

    Added as_underline function

--- src/colors.py
+++ src/colors.py
@@ -1,4 +1,4 @@
-"""Functions for displaying text with color."""
+"""Functions for displaying text with color and formatting."""
 
 color_codes = {'red': '31',
                 'blue': '34',
@@ -11,3 +11,6 @@ def as_color(text, color, bold=False):
     weight = 1 if bold else 0
     string = f'\033[{weight};{color_code}m{text}\033[0;m'
     return string
+
+def as_underline(text):
+    return f"\033[4m{text}\033[0m"
