ed3a1c2fe44cc3b34ab0a3d7b75fbfd98456670a
Marc Singleton <marcsingleton@berkeley.edu>
2024-10-12 12:36:16 -0400

    Added exception handling for interrupt to not print traceback

--- src/pydemic/__main__.py
+++ src/pydemic/__main__.py
@@ -1,3 +1,5 @@
+from sys import exit
+
 import pydemic.exceptions as exceptions
 from pydemic.main import main
 
@@ -5,3 +7,6 @@ try:
     main()
 except exceptions.GameOver as error:
     print('Game over:', error)
+except KeyboardInterrupt as error:
+    print()  # Start new line in case shell doesn't
+    exit()
